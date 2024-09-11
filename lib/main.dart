1d98fb098db7d941b78d47144007531d8a1b2038
Kai He <hekai@Kai-MacBookPro.local>
2024-09-11 14:51:11 +0800

    complete Sec06-04: column and row

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:[
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.grey,
+                child: Text('Container 1'),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: Text('Container2'),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Text('Container3'),
+              ),
+            ]
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
